0c1d73cb7934497a82d8315bd1836aeb44b7da89
Yggdrasil12 <juanmanuel120904@gmail.com>
2025-02-22 20:58:58 -0500

    first commit

---
+++ app/src/main/java/com/example/taller_2/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.taller_2
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
